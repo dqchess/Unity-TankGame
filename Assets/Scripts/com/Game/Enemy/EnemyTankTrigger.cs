c53c50586b32dcb8adba0270086a17f3399a9353
hindy66 <794310672@qq.com>
2020-02-17 17:07:45 +0800

    第一次管理unity项目

---
+++ Assets/Scripts/com/Game/Enemy/EnemyTankTrigger.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+/// <summary>
+/// 敌人坦克管理器类
+/// 
+/// </summary>
+public class EnemyTankTrigger : MonoBehaviour {
+
+    /// <summary>
+    /// 敌人随机数组
+    /// </summary>
+    public GameObject[] EnemyPrefabArr;
+
+    /// <summary>
+    /// 当前在线敌人坦克列表
+    /// </summary>
+    public static List<GameObject> EnemyList = new List<GameObject>();
+
+    /// <summary>
+    /// 敌人位置数组
+    /// </summary>
+    public GameObject[] EnemyPosArr;
+
+    //回合字幕
+    public Text roundText;
+    private int round=1;
+	
+	void Start () {
+        createEnemy();
+        StartCoroutine(RoundShow());
+	}
+
+    void Update()
+    {
+
+        if (EnemyList.Count == 0)
+        {
+            round++;
+            StartCoroutine(RoundShow());
+            createEnemy();
+            //StopCoroutine(RoundShow());
+        }
+    }
+
+    /// <summary>
+    /// 创建敌人坦克函数
+    /// </summary>
+    private void createEnemy()
+    {
+        for (int i = 0; i < EnemyPosArr.Length;i++ )
+        {
+            GameObject pos = EnemyPosArr[i];
+            //实例化
+            GameObject.Instantiate(EnemyPrefabArr[Random.Range(0,4)],pos.transform.position,pos.transform.rotation);
+        }
+    }
+
+    IEnumerator RoundShow ()
+    {
+        roundText.text = "Round " + round;
+        roundText.gameObject.SetActive(true);
+        yield return new WaitForSeconds(2);
+        roundText.gameObject.SetActive(false);
+    }
+
+}
---
+++ Assets/Scripts/com/Game/UI/AfterNamed.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AfterNamed : MonoBehaviour {
+    public Text name;
+
+    public GameObject warningPanel;
+    //玩家名字
+    public string playername;
+
+    //获得游戏数据保存脚本
+    public GameObject data;
+
+    //获取加载界面
+    public GameObject loadPanel;
+
+
+	// Use this for initialization
+	void Start () {
+        data = GameObject.FindGameObjectWithTag("Data");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+     public void  NullOrNot()
+    {
+        if(name.text=="")
+        {
+            Debug.Log("名为空");
+            StartCoroutine("Warning");
+        }
+        else
+        {
+            StartCoroutine("GetName");                    
+        }
+    }
+
+     IEnumerator  GetName()
+    {
+        playername = name.text;
+        data.GetComponent<SaveStartSceneData>().playername = playername;
+        loadPanel.SetActive(true);
+        yield return new WaitForSeconds(1);
+        Application.LoadLevel("Map");
+
+    }
+
+    public void InputNamePanelColse()
+    {
+        this.gameObject.SetActive(false);
+    }
+
+    IEnumerator Warning ()
+    {
+        warningPanel.SetActive(true);
+        yield return new WaitForSeconds(1);
+        warningPanel.SetActive(false);
+    }
+
+    
+}
